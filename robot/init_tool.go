4f70220324c77f172a200a96e5f71438110d81c5
Leexiaobu <leexiaobu.lix@gmail.com>
2022-10-10 10:00:02 +0800

    修改appkey

--- robot/init_tool.go
+++ robot/init_tool.go
@@ -8,20 +8,19 @@ import (
 
 // InitBotWithStart 系统启动的时候从Redis加载登录信息自动登录
 func InitBotWithStart() {
-	//keys, err := RedisClient.GetKeys("wechat:login:*")
-	//if err != nil {
-	//	logger.Log.Error("获取Key失败")
-	//	return
-	//}
 	logger.Log.Infof("登录管理员账户")
-	appKey := "wxboot:login:leexiaobu"
+	key := "wxboot:login:leexiaobu"
+	// 提取出AppKey
+	appKey := key[13:]
+	// 调用热登录
+	logger.Log.Debugf("当前热登录AppKey: %v", appKey)
 	bot := InitWechatBotHandle()
-	storage := protocol.NewRedisHotReloadStorage(appKey)
+	storage := protocol.NewRedisHotReloadStorage(key)
 	if err := bot.HotLogin(storage, true); err != nil {
 		logger.Log.Infof("[%v] 热登录失败，错误信息：%v", appKey, err.Error())
 		// 登录失败，删除热登录数据
-		if err := RedisClient.Del(appKey); err != nil {
-			logger.Log.Errorf("[%v] Redis缓存删除失败，错误信息：%v", appKey, err.Error())
+		if err := RedisClient.Del(key); err != nil {
+			logger.Log.Errorf("[%v] Redis缓存删除失败，错误信息：%v", key, err.Error())
 		}
 	}
 	loginUser, _ := bot.GetCurrentUser()
